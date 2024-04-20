2ed4d6a0362dd05d1cd12b38f01d760eb2fd86d2
KevinWang <69309534+kevinwqw@users.noreply.github.com>
2024-04-20 14:44:25 +0800

    update project config

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App.tsx'
-import './index.module.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development'
+
+  return {
+    base: '/',
+    mode,
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src')
+      }
+    },
+    plugins: [
+      react(),
+      svgr({
+        include: '**/*.svg?react',
+        exclude: ''
+      })
+    ],
+    esbuild: { drop: isDev ? [] : ['console', 'debugger'] },
+    server: {
+      port: 3000
+    }
+  }
 })
